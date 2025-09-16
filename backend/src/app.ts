0fc6d9765dd848f4a89af638f3c5f6f8f334e636
NoSpooksAllowed <hersgori4@protonmail.com>
2025-09-16 13:30:09 -0300

    Updated version for /api/version endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -30,7 +30,7 @@ app.get("/", function (_req, res) {
 });
 
 app.get("/api/version", function (_req, res) {
-  return res.send("Version 1");
+  return res.send("Version 2");
 });
 
 app.use(authErrorHandler, prismaErrorHandler, generalErrorHandler);
