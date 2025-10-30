5fb8b797dc0dcc089121e8b6a3d81ea6e7dd5493
NoSpooksAllowed <hersgori4@protonmail.com>
2025-10-30 03:16:13 -0300

    modified deploy bd script

--- k8s/scripts/deploy_bd.sh
+++ k8s/scripts/deploy_bd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "=== Deploy PostgreSQL ==="
-kubectl apply -f secrets/postgres-secrets.yaml
-kubectl apply -f base/db/
+kubectl apply -f db/
 
 kubectl wait --for=condition=ready pod -l app=postgres --timeout=120s -n realworld-app
